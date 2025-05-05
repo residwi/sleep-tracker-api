ac1056f96b23841edd5633b18375f8dd2855e451
Resi Dwi <37761892+residwi@users.noreply.github.com>
2025-05-06 01:12:36 +0700

    feat(follows): add follower/following system migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_05_144344) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_05_173044) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "follows", force: :cascade do |t|
+    t.bigint "followed_id", null: false
+    t.bigint "follower_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["followed_id", "follower_id"], name: "index_follows_on_followed_id_and_follower_id", unique: true
+    t.index ["followed_id"], name: "index_follows_on_followed_id"
+    t.index ["follower_id"], name: "index_follows_on_follower_id"
+  end
 
   create_table "sessions", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -42,6 +52,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_05_144344) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "follows", "users", column: "followed_id"
+  add_foreign_key "follows", "users", column: "follower_id"
   add_foreign_key "sessions", "users"
   add_foreign_key "sleep_records", "users"
 end
