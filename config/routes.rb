42e3c67669890df996117f3d1cbf564d00c8cd9b
Resi Dwi <37761892+residwi@users.noreply.github.com>
2025-05-06 23:27:37 +0700

    refactor(routes): update root path to use feeds#index
    Removed HomeController and its associated spec as they are no longer needed

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  root "home#index"
+  root "feeds#index"
 
   resources :sleep_records
   resources :users, only: [ :index, :show ] do
