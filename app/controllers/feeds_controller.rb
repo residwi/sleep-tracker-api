9d9688679c76a2182fa82b00144fad47370261c1
Resi Dwi <37761892+residwi@users.noreply.github.com>
2025-05-07 22:06:52 +0700

    refactor(feeds): update feeds logic of sleep records
    Update the logic to return sleep records of the following user from the
    past week and sorted by duration

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,13 +1,18 @@
 class FeedsController < ApplicationController
   def index
     following_ids = Current.user.following.pluck(:id)
-    following_ids << Current.user.id
 
-    @feeds = SleepRecord.where(user_id: following_ids)
-      .order(created_at: :desc)
+    @feeds = SleepRecord.includes(:user)
+      .where(user_id: following_ids)
+      .where("start_time >= ?", 1.week.ago)
+      .order(duration: :desc)
       .select(:id, :start_time, :end_time, :duration, :user_id)
 
     @pagy, @records = pagy_keyset(@feeds)
+    @records = @records.map do |record|
+      record.as_json(only: [ :id, :start_time, :end_time, :duration ])
+            .merge(user: { id: record.user.id, name: record.user.name })
+    end
 
     json_pagination_response(@records, @pagy)
   end
--- spec/requests/feeds_spec.rb
+++ spec/requests/feeds_spec.rb
@@ -14,19 +14,28 @@ RSpec.describe "Feeds", type: :request do
       @authentication_header = { Authorization: "Bearer #{token}" }
     end
 
-    it "returns the feeds of the current user and their following" do
-      sleep_records = create_list(:sleep_record, 2, user: user)
-      other_sleep_records = other_users.map do |other_user|
-        create_list(:sleep_record, 2, user: other_user)
-      end
+    it "returns the feeds of the current user and their following from the previous week, sorted by duration" do
+      one_week_ago = 1.week.ago
+      two_weeks_ago = 2.weeks.ago
+
+      sleep_record_8_hours = create(:sleep_record, user: other_users[0], start_time: one_week_ago + 1.day, end_time: one_week_ago + 1.day + 8.hours)
+      sleep_record_6_hours = create(:sleep_record, user: other_users[1], start_time: one_week_ago + 2.days, end_time: one_week_ago + 2.days + 6.hours)
+      sleep_record_9_hours = create(:sleep_record, user: other_users[2], start_time: one_week_ago + 3.days, end_time: one_week_ago + 3.days + 9.hours)
+      create(:sleep_record, user: other_users[0], start_time: two_weeks_ago, end_time: two_weeks_ago + 7.hours)
+      create(:sleep_record, user: user, start_time: one_week_ago + 1.day, end_time: one_week_ago + 1.day + 7.hours)
 
       get feeds_path, headers: @authentication_header
 
       json_response = JSON.parse(response.body)
       expect(response).to have_http_status(:success)
-      expect(json_response["data"].size).to eq(8)
-      expect(json_response["data"].map { |record| record["id"] }).to match_array(sleep_records.map(&:id) + other_sleep_records.flatten.map(&:id))
-      expect(json_response["data"].flat_map(&:keys).uniq).to match_array([ "id", "start_time", "end_time", "duration", "user_id", "created_at" ])
+      expect(json_response["data"].size).to eq(3)
+      expect(json_response["data"][0]["id"]).to eq(sleep_record_9_hours.id)
+      expect(json_response["data"][1]["id"]).to eq(sleep_record_8_hours.id)
+      expect(json_response["data"][2]["id"]).to eq(sleep_record_6_hours.id)
+      expect(json_response["data"][0]["user"]).to be_present
+      expect(json_response["data"][0]["user"]["id"]).to eq(other_users[2].id)
+      expect(json_response["data"][0]["user"]["name"]).to eq(other_users[2].name)
+      expect(json_response["data"].flat_map(&:keys).uniq).to match_array([ "id", "start_time", "end_time", "duration", "user" ])
     end
 
     it "returns an empty array if no feeds are available" do
@@ -40,14 +49,22 @@ RSpec.describe "Feeds", type: :request do
 
     context "when paginated" do
       it "returns paginated feeds" do
-        sleep_records = create_list(:sleep_record, 10, user: user)
+        sleep_records = 10.times.map do |i|
+          i += 1
+
+          create(:sleep_record,
+            user: other_users[i % 3],
+            start_time: 1.week.ago + i.hours,
+            end_time: 1.week.ago + i.hours + (10 - i).hours
+          )
+        end
 
         get feeds_path(limit: 5), headers: @authentication_header
 
         json_response = JSON.parse(response.body)
         expect(response).to have_http_status(:success)
         expect(json_response["data"].size).to eq(5)
-        expect(json_response["data"].map { |record| record["id"] }).to match_array(sleep_records.sort_by(&:created_at).reverse.first(5).map(&:id))
+        expect(json_response["data"].map { |record| record["id"] }).to match_array(sleep_records.sort_by(&:duration).reverse.first(5).map(&:id))
         expect(json_response["pagination"]["next"]).to be_present
       end
     end
